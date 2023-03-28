2ef6fc209f8a79b23fc23551cc5b3bb440ead3d2
MinhLamp <128891571+MinhLamp@users.noreply.github.com>
2023-03-28 14:47:46 +0700

    Add files via upload
    Thêm bình luận

--- comment.php
+++ comment.php
@@ -1,21 +1,57 @@
 <?php
-function addCmt($name,$iduser,$idsp,$noidung){
-    $sql = "INSERT INTO binhluan (name, iduser, idsp, noidung) VALUES ('$name', '$iduser', '$idsp', '$noidung')";
-    $conn=connect();
-    $conn->exe($sql);
-    $productName = $_POST["name"];
-    $productDesc = $_POST["desc"];
-    $view = $_POST['view'];
-    $file = $_FILES['image'];
-    $filename = "";
-}
+    session_start();
+    include "../model/connect.php";
+    include "../model/comment.php";
+    if(isset($_SESSION['sid'])&&($_SESSION['sid']>0)){
 
-function showcmt(){
-    $sql = "select * from comment order bt id desc";
-    $conn=connect();
-    $stmt= $conn->prepare($sql);
-    $stmt->execute();
-    $stmt->setFechMode(PDO::FETCH_ASSOC);
-    return $stmt->fetchAll();
-}
+        if(isset($_SESSION['user'])&&($_SESSION['user']!="")){
+            $user=$_SESSION['user'];
+        }else{
+            $user="";
+        }
+
+        if(isset($_POST['guibinhluan'])&&($_POST['guibinhluan'])){
+            //
+                $name=$_POST['name'];
+                $name=$_POST['noidung'];
+                $idsp=$_POST['idsp'];
+                $iduser=$_SESSION['sid'];
+            //
+
+            addCmt($name,$iduser,$idsp,$noidung);
+                
+        }
+        $dsbl=showcmt();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments</title>
+</head>
+<body>
+    <hr>
+    <form action="comment.php">
+        <input type="text" name="name" value="<?=$user?>"> 
+        <input type="hidden" name="idsp" value="<?=$_GET['idsp']?>">
+        <textarea name="noidung" id="" cols="30" rows="10" name="noidung"></textarea>
+        <input type="submit" value="Gửi bình luận" name="guibinhluan">
+    </form>
+
+    <hr>
+        <?php 
+            
+            foreach ($dsbl as $bl) {
+                echo $bl['name'].' - '.$bl['noidung']."<br>";
+            }
+
+    ?>
+
+    
+</body>
+</html>
+<?php }else{
+    echo "<a href='index.php?act=user' target='_parent'>Bạn vui lòng đăng nhập!</a>";
+}?>
